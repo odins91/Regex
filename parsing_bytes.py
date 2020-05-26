9dcb544c21e864768849cc62a628e491e2919ab4
odins91 <31848027+odins91@users.noreply.github.com>
2020-05-26 21:42:08 +0200

    First version of the app

--- parsing_bytes.py
+++ parsing_bytes.py
@@ -1,4 +1,19 @@
 import re
 
 def parse_bytes(input):
-    pass
+    data = re.compile(r'\d[0-1]*')
+    return data.findall(input)
+
+
+    # Approach with one input:
+
+    # merged = data.search(input)
+    # if merged:
+    #     return merged.group()
+    # return None
+
+
+print(parse_bytes("01010101"))
+print(parse_bytes("asahasa"))
+print(parse_bytes("01010101 asahasa"))
+
